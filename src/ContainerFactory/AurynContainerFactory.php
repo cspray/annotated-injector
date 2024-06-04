cspray/annotated-container

Task/remove psr log (#355)
* Begin emitting events to replace logging

* Remove remaining configuration references

* Finish replacing logging with event system

src/ContainerFactory/AurynContainerFactory.php
@@ -10,7 +10,6 @@ use Cspray\AnnotatedContainer\Autowire\AutowireableInvoker;
 use Cspray\AnnotatedContainer\Autowire\AutowireableParameter;
 use Cspray\AnnotatedContainer\Autowire\AutowireableParameterSet;
 use Cspray\AnnotatedContainer\ContainerFactory\AliasResolution\AliasDefinitionResolution;
-use Cspray\AnnotatedContainer\Definition\ConfigurationDefinition;
 use Cspray\AnnotatedContainer\Definition\InjectDefinition;
 use Cspray\AnnotatedContainer\Definition\ServiceDefinition;
 use Cspray\AnnotatedContainer\Definition\ServiceDelegateDefinition;
@@ -31,10 +30,6 @@ if (!class_exists(Injector::class)) {
  * A ContainerFactory that utilizes the rdlowrey/auryn Container as its backing implementation.
  */
 final class AurynContainerFactory extends AbstractContainerFactory implements ContainerFactory {
-
-    protected function getBackingContainerType() : ObjectType {
-        return objectType(Injector::class);
-    }
 
     protected function getContainerFactoryState() : AurynContainerFactoryState {
         return new AurynContainerFactoryState();
@@ -83,51 +78,24 @@ final class AurynContainerFactory extends AbstractContainerFactory implements Co
         assert($state instanceof AurynContainerFactoryState);
         $injectTargetType = $definition->getTargetIdentifier()->getClass()->getName();
 
-        if ($definition->getTargetIdentifier()->isMethodParameter()) {
-            $method = $definition->getTargetIdentifier()->getMethodName();
-            $parameterName = $definition->getTargetIdentifier()->getName();
+        $method = $definition->getTargetIdentifier()->getMethodName();
+        $parameterName = $definition->getTargetIdentifier()->getName();
 
-            $value = $this->getInjectDefinitionValue($definition);
-            if ($value instanceof ContainerReference) {
-                $key = $parameterName;
-                $nameType = $state->getTypeForName($value->name);
-                if ($nameType !== null) {
-                    $value = $nameType->getName();
-                } else {
-                    $value = $value->name;
-                }
+        $value = $this->getInjectDefinitionValue($definition);
+        if ($value instanceof ContainerReference) {
+            $key = $parameterName;
+            $nameType = $state->getTypeForName($value->name);
+            if ($nameType !== null) {
+                $value = $nameType->getName();
             } else {
-                $key = ':' . $parameterName;
+                $value = $value->name;
             }
-
-            $state->addMethodInject($injectTargetType, $method, $key, $value);
         } else {
-            $property = $definition->getTargetIdentifier()->getName();
-            $value = $this->getInjectDefinitionValue($definition);
-            $state->addPropertyInject($injectTargetType, $property, $value);
+            $key = ':' . $parameterName;
         }
 
-    }
+        $state->addMethodInject($injectTargetType, $method, $key, $value);
 
-    protected function handleConfigurationDefinition(ContainerFactoryState $state, ConfigurationDefinition $definition) : void {
-        assert($state instanceof AurynContainerFactoryState);
-        $state->injector->share($definition->getClass()->getName());
-        if ($definition->getName() !== null) {
-            $state->addNameType($definition->getName(), $definition->getClass());
-        }
-
-        if (!method_exists($definition->getClass()->getName(), '__construct')) {
-            $state->injector->delegate($definition->getClass()->getName(), static function() use($definition, $state) {
-                $configReflection = (new \ReflectionClass($definition->getClass()->getName()));
-                $configInstance = $configReflection->newInstanceWithoutConstructor();
-                $properties = $state->propertiesToInject($definition->getClass()->getName());
-                foreach ($properties as $prop => $value) {
-                    $reflectionProperty = $configReflection->getProperty($prop);
-                    $reflectionProperty->setValue($configInstance, $value);
-                }
-                return $configInstance;
-            });
-        }
     }
 
     protected function createAnnotatedContainer(ContainerFactoryState $state, Profiles $activeProfiles) : AnnotatedContainer {

cspray/annotated-injector

src/ContainerFactory/AurynContainerFactory.php
@@ -10,7 +10,6 @@ use Cspray\AnnotatedContainer\Autowire\AutowireableInvoker;
 use Cspray\AnnotatedContainer\Autowire\AutowireableParameter;
 use Cspray\AnnotatedContainer\Autowire\AutowireableParameterSet;
 use Cspray\AnnotatedContainer\ContainerFactory\AliasResolution\AliasDefinitionResolution;
-use Cspray\AnnotatedContainer\Definition\ConfigurationDefinition;
 use Cspray\AnnotatedContainer\Definition\ContainerDefinition;
 use Cspray\AnnotatedContainer\Definition\InjectDefinition;
 use Cspray\AnnotatedContainer\Definition\ServiceDefinition;
@@ -85,38 +84,11 @@ final class AurynContainerFactory extends AbstractContainerFactory implements Co
     protected function handleInjectDefinition(ContainerFactoryState $state, InjectDefinition $definition) : void {
         assert($state instanceof AurynContainerFactoryState);
         $injectTargetType = $definition->getTargetIdentifier()->getClass()->getName();
+        $method = $definition->getTargetIdentifier()->getMethodName();
+        $parameterName = $definition->getTargetIdentifier()->getName();
+        $value = $this->getInjectDefinitionValue($definition);
 
-        if ($definition->getTargetIdentifier()->isMethodParameter()) {
-            $method = $definition->getTargetIdentifier()->getMethodName();
-            $parameterName = $definition->getTargetIdentifier()->getName();
-            $value = $this->getInjectDefinitionValue($definition);
-            $state->addMethodInject($injectTargetType, $method, $parameterName, $value);
-        } else {
-            $property = $definition->getTargetIdentifier()->getName();
-            $value = $this->getInjectDefinitionValue($definition);
-            $state->addPropertyInject($injectTargetType, $property, $value);
-        }
-    }
-
-    protected function handleConfigurationDefinition(ContainerFactoryState $state, ConfigurationDefinition $definition) : void {
-        assert($state instanceof AurynContainerFactoryState);
-        $state->injector->share($definition->getClass()->getName());
-        if ($definition->getName() !== null) {
-            $state->addNameType($definition->getName(), $definition->getClass());
-        }
-
-        if (!method_exists($definition->getClass()->getName(), '__construct')) {
-            $state->injector->delegate($definition->getClass()->getName(), static function() use($definition, $state) {
-                $configReflection = (new \ReflectionClass($definition->getClass()->getName()));
-                $configInstance = $configReflection->newInstanceWithoutConstructor();
-                $properties = $state->propertiesToInject($definition->getClass()->getName());
-                foreach ($properties as $prop => $value) {
-                    $reflectionProperty = $configReflection->getProperty($prop);
-                    $reflectionProperty->setValue($configInstance, $value);
-                }
-                return $configInstance;
-            });
-        }
+        $state->addMethodInject($injectTargetType, $method, $parameterName, $value);
     }
 
     protected function createAnnotatedContainer(ContainerFactoryState $state, Profiles $activeProfiles) : AnnotatedContainer {